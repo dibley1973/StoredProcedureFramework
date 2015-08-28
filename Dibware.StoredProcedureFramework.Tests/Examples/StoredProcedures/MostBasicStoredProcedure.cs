9abe167904cb047a14f335fd26c453afa7bc395c
dibley1973 <dibley1973@yahoo.co.uk>
2015-08-28 08:13:55 +0100

    20150828_0813_DWDT Started work on examples for teh documentation.

---
+++ Dibware.StoredProcedureFramework.Tests/Examples/StoredProcedures/MostBasicStoredProcedure.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Dibware.StoredProcedureFramework.Tests.Examples.StoredProcedures
+{
+    internal class MostBasicStoredProcedure2
+        : StoredProcedureBase<NullStoredProcedureResult, NullStoredProcedureParameters>
+    {
+        public MostBasicStoredProcedure2()
+            : base(new NullStoredProcedureParameters())
+        {
+        }
+    }
+
+
+    internal class MostBasicStoredProcedure
+        : NoParametersNoReturnTypeStoredProcedureBase
+    {
+        
+    }
+}
