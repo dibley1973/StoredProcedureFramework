9dc4ab05b508313c60925738e799ad05a277fd83
dibley1973 <dibley1973@yahoo.co.uk>
2015-08-31 07:22:42 +0100

    20150831_0722_DWDT Added example code for the WIKI pages into the test project .

---
+++ Dibware.StoredProcedureFramework.Tests/Examples/StoredProcedures/StoredProcedureWithParametersButNoReturn.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Dibware.StoredProcedureFramework.Tests.Examples.StoredProcedures
+{
+    internal class StoredProcedureWithParametersButNoReturn2
+        : StoredProcedureBase<NullStoredProcedureResult, StoredProcedureWithParametersButNoReturnParameters>
+    {
+        public StoredProcedureWithParametersButNoReturn2(StoredProcedureWithParametersButNoReturnParameters parameters)
+            : base(parameters)
+        {
+        }
+    }
+
+    internal class StoredProcedureWithParametersButNoReturn
+        : NoReturnTypeStoredProcedureBase<StoredProcedureWithParametersButNoReturnParameters>
+    {
+        public StoredProcedureWithParametersButNoReturn(StoredProcedureWithParametersButNoReturnParameters parameters)
+            : base(parameters)
+        {
+        }
+    }
+
+
+    internal class StoredProcedureWithParametersButNoReturnParameters
+    {
+        int Id { get; set; }
+    }
+}
---
+++ Dibware.StoredProcedureFramework.Tests/Examples/StoredProcedures/StoredProcedureWithoutParameters.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Dibware.StoredProcedureFramework.Tests.Examples.StoredProcedures
+{
+    internal class StoredProcedureWithoutParameters2
+        : StoredProcedureBase<StoredProcedureWithoutParametersReturntype, NullStoredProcedureParameters>
+    {
+        public StoredProcedureWithoutParameters2()
+            : base(new NullStoredProcedureParameters())
+        {
+        }
+    }
+
+    internal class StoredProcedureWithoutParameters
+        : NoParametersStoredProcedureBase<StoredProcedureWithoutParametersReturntype>
+    {
+    }
+
+
+    internal class StoredProcedureWithoutParametersReturntype
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+    }
+}
