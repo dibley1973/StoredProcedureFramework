20a18379c57c208456e05e4a40d40f9ce96ebaee
dibley1973 <dibley1973@yahoo.co.uk>
2015-09-24 07:48:59 +0100

    20150924_0748_DWDT Started work towards handling mutiple recordsets returned from stored procedure

---
+++ Dibware.StoredProcedureFramework/ReturnTypeCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dibware.StoredProcedureFramework
+{
+    /// <summary>
+    /// Contains a list of return types for a stored procedure
+    /// That returns multiple record sets.
+    /// </summary>
+    public class ReturnTypeCollection : List<Type>
+    {
+    }
+}
