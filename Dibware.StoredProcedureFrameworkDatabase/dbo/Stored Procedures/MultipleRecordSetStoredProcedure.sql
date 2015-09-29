eac2ec00247f62493284e5f08650c0613a1ba657
dibley1973 <dibley1973@yahoo.co.uk>
2015-09-29 21:24:04 +0100

    20150929_2123_DWDT just remembered to synch database project! Doh!

---
+++ Dibware.StoredProcedureFrameworkDatabase/dbo/Stored Procedures/MultipleRecordSetStoredProcedure.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE [dbo].[MultipleRecordSetStoredProcedure]
+    @Id                 INT
+,   @Name               VARCHAR(20)
+,   @Active             BIT
+,   @Price              DECIMAL(10, 4)
+,   @UniqueIdentifier   UNIQUEIDENTIFIER
+,   @Count              TINYINT
+AS
+BEGIN
+    /* First Record Set */
+    SELECT 
+        @Id     AS Id
+    ,   @Name   AS Name
+    UNION
+    SELECT
+        17      AS Id
+    ,   'Bill'  AS Name;
+
+    /* Second Record Set */
+    SELECT 
+        @Active as Active
+    ,   @Price  AS Price
+    --UNION
+    --SELECT
+    --    CAST(0 AS BIT)                  AS Active
+    --,   CAST(99.99 AS DECIMAL(10,4))    AS Price
+
+    /* Third Record Set */
+    SELECT
+        @UniqueIdentifier   AS [UniqueIdentifier]
+    ,   @Count              AS [Count]
+    
+END
