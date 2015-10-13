13f9d3415171ec502a6a93503ea4c5b1c828198b
dibley1973 <dibley1973@yahoo.co.uk>
2015-10-13 07:47:49 +0100

    20151013_0747_DWDT Included untracked files!

---
+++ Dibware.StoredProcedureFramework.Tests/Integration Tests/DbContextTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity;
+
+namespace Dibware.StoredProcedureFramework.Tests.Integration_Tests
+{
+    [TestClass]
+    public class DbContextTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+    public class TestContext : DbContext
+    {
+        //public MostBasicEFStoredProcedure MostBasicStoredProcedure { get; set; }
+
+        //public TestContext()
+        //{
+        //    MostBasicStoredProcedure = new MostBasicEFStoredProcedure(this);
+        //}
+    }
+}
---
+++ Dibware.StoredProcedureFrameworkForEF/StoredProcedureBase.cs
@@ -0,0 +1,125 @@
+﻿using Dibware.StoredProcedureFramework.Contracts;
+using System;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+
+namespace Dibware.StoredProcedureFrameworkForEF
+{
+    /// <summary>
+    /// Expands upon functionality of the <see cref="Dibware.StoredProcedureFramework.Base.StoredProcedureBase{TReturn, TParameters}"/> 
+    /// class by adding support for execution from DbContext by providing
+    /// constructors which take a dbcontext.
+    /// </summary>
+    /// <typeparam name="TReturn">The type of the return.</typeparam>
+    /// <typeparam name="TParameters">The type of the parameters.</typeparam>
+    public abstract class StoredProcedureBase<TReturn, TParameters>
+        : StoredProcedureFramework.Base.StoredProcedureBase<TReturn, TParameters>
+        where TReturn : class, new()
+        where TParameters : class
+    {
+        #region Fields
+
+        private readonly DbContext _context;
+
+        #endregion
+
+        #region Constructors
+
+        ///// <summary>
+        ///// Initializes a new instance of the <see cref="StoredProcedureBase{TReturn, TParameters}" /> class.
+        ///// Sets the procedure name to match the stored procedure class.
+        ///// </summary>
+        ///// <param name="context">The context.</param>
+        //private StoredProcedureBase(DbContext context)
+        //    : base()
+        //{
+        //    _context = context;
+        //}
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StoredProcedureBase{TReturn, TParameters}" />
+        /// class with parameters. This is the minimum requirement for constructing
+        /// a stored procedure.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="parameters">The parameters.</param>
+        protected StoredProcedureBase(DbContext context, TParameters parameters)
+            : base(parameters)
+        {
+            // Validate arguments
+            if (context == null) throw new ArgumentNullException("context");
+
+            _context = context;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StoredProcedureBase{TReturn, TParameters}" />
+        /// class with parameters and procedure name.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="procedureName">Name of the procedure.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <exception cref="System.ArgumentNullException">context</exception>
+        protected StoredProcedureBase(DbContext context,
+            string procedureName,
+            TParameters parameters)
+            : base(procedureName, parameters)
+        {
+            // Validate arguments
+            if (context == null) throw new ArgumentNullException("context");
+
+            _context = context;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StoredProcedureBase{TReturn, TParameters}" />
+        /// class with parameters, schema name and procedure name.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="schemaName">Name of the schema.</param>
+        /// <param name="procedureName">Name of the procedure.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <exception cref="System.ArgumentNullException">context</exception>
+        protected StoredProcedureBase(DbContext context,
+            string schemaName,
+            string procedureName,
+            TParameters parameters)
+            : base(procedureName, parameters)
+        {
+            // Validate arguments
+            if (context == null) throw new ArgumentNullException("context");
+
+            _context = context;
+        }
+
+        #endregion
+
+        #region Members
+
+        /// <summary>
+        /// Executes this instance against the DbContext which this instance was
+        /// constructred with.
+        /// </summary>
+        /// <param name="commandTimeout">The command timeout. [optional]</param>
+        /// <param name="commandBehavior">The command behavior. [optional]</param>
+        /// <param name="transaction">The transaction. [optional]</param>
+        /// <returns></returns>
+        public TReturn Execute(
+            int? commandTimeout = null,
+            CommandBehavior commandBehavior = CommandBehavior.Default,
+            SqlTransaction transaction = null)
+        {
+            IStoredProcedure<TReturn, TParameters> storedProcedure = this;
+            TReturn result = _context.ExecuteStoredProcedure<TReturn, TParameters>(
+                storedProcedure,
+                commandTimeout,
+                commandBehavior,
+                transaction);
+
+            return result;
+        }
+
+        #endregion
+    }
+}
