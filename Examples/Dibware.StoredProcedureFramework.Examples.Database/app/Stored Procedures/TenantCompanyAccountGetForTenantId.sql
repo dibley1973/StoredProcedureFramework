c851e4f3df7af97a1d4db3fe42acb2bc5e13c185
dibley1973 <dibley1973@yahoo.co.uk>
2015-11-23 18:18:17 +0000

    20151123_1817_DWDT Added multiple recordset stored proc to examples database project

---
+++ Examples/Dibware.StoredProcedureFramework.Examples.Database/app/Stored Procedures/TenantCompanyAccountGetForTenantId.sql
@@ -0,0 +1,45 @@
+﻿
+--DROP PROCEDURE app.TenantCompanyAccountGetForTenantId
+--go
+CREATE PROCEDURE app.TenantCompanyAccountGetForTenantId
+    @TenantId int
+AS
+BEGIN
+    SELECT
+        [TenantId]
+    ,   [IsActive]
+    ,   [TenantName]
+    ,   [RecordCreatedDateTime]
+    FROM
+        [app].[Tenant]
+    WHERE
+        [TenantId] = @TenantId;
+
+    SELECT 
+        [CompanyId]
+    ,   [TenantId]
+    ,   [IsActive]
+    ,   [CompanyName]
+    ,   [RecordCreatedDateTime]
+    FROM
+        [app].[Company]
+    WHERE
+        [TenantId] = @TenantId;
+
+    SELECT 
+        [AccountId]
+    ,   [company].[CompanyId]
+    ,   [account].[IsActive]
+    ,   [AccountName]
+    ,   [account].[RecordCreatedDateTime]
+    ,   [LastUpdatedDateTime]
+    FROM
+        [app].[Account] [account]
+    INNER JOIN
+        [app].[Company] [company]
+    ON 
+        [company].[CompanyId] = [account].[CompanyId]
+    AND
+        [company].[TenantId] = @TenantId;
+
+END
