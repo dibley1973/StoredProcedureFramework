0c7acf848baf4cb751dfe4ce5c7e65bad404f905
dibley1973 <dibley1973@yahoo.co.uk>
2015-12-02 08:11:42 +0000

    20151202_0811_DWDT Started work on StoredProcedureDbCommandCreator

---
+++ CodeBase/Dibware.StoredProcedureFramework/Helpers/Base/DbCommandCreatorBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace Dibware.StoredProcedureFramework.Helpers.Base
+{
+    public abstract class DbCommandCreatorBase
+    {
+        #region Constructor
+
+        protected DbCommandCreatorBase(DbConnection connection)
+        {
+            if (connection == null) throw new ArgumentNullException("connection");
+
+
+            _connection = connection;
+        }
+
+        protected DbCommandCreatorBase(DbConnection connection,
+            IEnumerable<SqlParameter> parameters)
+            : this(connection)
+        {
+            if (parameters == null) throw new ArgumentNullException("parameters");
+
+
+            _parameters = parameters;
+        }
+
+
+        #endregion
+
+
+        #region Public Members
+
+        public abstract void BuildCommand();
+
+        /// <summary>
+        /// Gets the command or null if it has not been built.
+        /// </summary>
+        /// <value>
+        /// The command.
+        /// </value>
+        public DbCommand Command
+        {
+            get { return _command; }
+        }
+
+        #endregion
+
+        #region Private Members
+
+        private DbCommand _command;
+        private DbConnection _connection;
+        private IEnumerable<SqlParameter> _parameters;
+        protected int? _commandTimeout = null;
+        private SqlTransaction _transaction = null;
+
+        #endregion
+    }
+}
---
+++ CodeBase/Dibware.StoredProcedureFramework/Helpers/StoredProcedureDbCommandCreator.cs
@@ -0,0 +1,82 @@
+﻿using Dibware.StoredProcedureFramework.Helpers.Base;
+using System;
+using System.Data.Common;
+
+namespace Dibware.StoredProcedureFramework.Helpers
+{
+    public class StoredProcedureDbCommandCreator
+        : DbCommandCreatorBase
+    {
+        #region Constructor
+
+        protected StoredProcedureDbCommandCreator(DbConnection connection)
+            : base(connection)
+        {
+
+        }
+
+
+        #endregion
+
+        #region Public Members
+
+        public override void BuildCommand()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Factory Methods
+
+        public static StoredProcedureDbCommandCreator CreateDbCommandCreator(
+            DbConnection connection,
+            string procedureName)
+        {
+            if (connection == null) throw new ArgumentNullException("connection");
+            if (string.IsNullOrWhiteSpace(procedureName)) throw new ArgumentNullException("procedureName");
+
+            var builder = new StoredProcedureDbCommandCreator(connection)
+                .WithProcedureName(procedureName);
+
+            return builder;
+        }
+
+        public static StoredProcedureDbCommandCreator CreateDbCommandCreatorWithCommandTimeout(
+            DbConnection connection,
+            string procedureName,
+            int commandTimeout)
+        {
+            if (connection == null) throw new ArgumentNullException("connection");
+            if (string.IsNullOrWhiteSpace(procedureName)) throw new ArgumentNullException("procedureName");
+
+            var builder = new StoredProcedureDbCommandCreator(connection)
+                .WithProcedureName(procedureName)
+                .WithCommandTimeout(commandTimeout);
+
+            return builder;
+        }
+
+
+        #endregion
+
+        #region Private Members
+
+        private StoredProcedureDbCommandCreator WithProcedureName(string value)
+        {
+            _procedureName = value;
+            return this;
+        }
+
+        private StoredProcedureDbCommandCreator WithCommandTimeout(int value)
+        {
+            _commandTimeout = value;
+            return this;
+        }
+
+
+        private string _procedureName;
+
+        #endregion
+    }
+}
