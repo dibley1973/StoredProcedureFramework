1483105964944f193931ed6cca2372a3e91490fd
dibley1973 <dibley1973@yahoo.co.uk>
2015-12-11 07:55:46 +0000

    20151211_0755_DWDT Start of v0.7 where intention is to add support for Sql UDFs

--- Tests/Dibware.StoredProcedureFramework.IntegrationTests.Database/Properties/AssemblyInfo.cs
+++ Tests/Dibware.StoredProcedureFramework.IntegrationTests.Database/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
