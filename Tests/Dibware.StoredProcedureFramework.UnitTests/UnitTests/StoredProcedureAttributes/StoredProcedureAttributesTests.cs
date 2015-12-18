04a3120f2df67b78844dbcee0b42b20bed395490
dibley1973 <dibley1973@yahoo.co.uk>
2015-12-18 08:04:54 +0000

    20151218_0804_DWDT Started to add `StoredProcedureAttributesTests`

---
+++ Tests/Dibware.StoredProcedureFramework.UnitTests/UnitTests/StoredProcedureAttributes/StoredProcedureAttributesTests.cs
@@ -0,0 +1,46 @@
+﻿using Dibware.StoredProcedureFramework.StoredProcedureAttributes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+
+namespace Dibware.StoredProcedureFramework.Tests.UnitTests.StoredProcedureAttributes
+{
+    [TestClass]
+    public class StoredProcedureAttributesTests
+    {
+        #region ParameterDirection
+
+        [TestMethod]
+        public void Value_WhenDirectionAttributeConstructedWithValidValue_ReturnsConstructedValue()
+        {
+            // ARRANGE
+            const ParameterDirection expectedValue = ParameterDirection.InputOutput;
+
+            // ACT
+            var directionAttribute = new DirectionAttribute(expectedValue);
+            var actualValue = directionAttribute.Value;
+
+            // ASSERT
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        #endregion
+
+        #region NameAttribute
+
+        [TestMethod]
+        public void Value_WhenNameAttributeConstructedWithValidValue_ReturnsConstructedValue()
+        {
+            // ARRANGE
+            const string expectedValue = "Test";
+
+            // ACT
+            var directionAttribute = new NameAttribute(expectedValue);
+            var actualValue = directionAttribute.Value;
+
+            // ASSERT
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        #endregion
+    }
+}
