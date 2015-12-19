e372086571703523a6a0361845cca25e0584f36a
dibley1973 <dibley1973@yahoo.co.uk>
2015-12-19 06:24:20 +0000

    20151219_0623_DWDT Started work on `PropertyNameHelper` class and test

---
+++ CodeBase/Dibware.StoredProcedureFramework/Helpers/PropertyNameHelper.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Dibware.StoredProcedureFramework.Extensions;
+using Dibware.StoredProcedureFramework.StoredProcedureAttributes;
+using System.Reflection;
+
+namespace Dibware.StoredProcedureFramework.Helpers
+{
+    public class PropertyNameHelper
+    {
+        #region Fields
+
+        private readonly PropertyInfo _property;
+        private string _name;
+        private NameAttribute _nameAttribute;
+
+        #endregion
+
+        #region Construtors
+
+        public PropertyNameHelper(PropertyInfo property)
+        {
+            if(property == null) throw new ArgumentNullException("property");
+
+            _property = property;
+        }
+
+        #endregion
+
+        #region Public Members
+
+        public PropertyNameHelper Build()
+        {
+            SetNameAttributeIfExists();
+            SetName();
+            return this;
+        }
+
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        #endregion
+
+        #region Private members
+
+        private bool HasNameAttribute
+        {
+            get { return _nameAttribute != null; }
+        }
+
+        private void SetName()
+        {
+            if (HasNameAttribute)
+            {
+                SetNameFromAttribute();
+            }
+            else
+            {
+                SetNameFromPropertyName();
+            }
+        }
+
+        private void SetNameFromAttribute()
+        {
+            _name = _nameAttribute.Value;
+        }
+
+        private void SetNameFromPropertyName()
+        {
+            _name = _property.Name;
+        }
+
+        private void SetNameAttributeIfExists()
+        {
+            _nameAttribute = _property.GetAttribute<NameAttribute>();
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/Dibware.StoredProcedureFramework.UnitTests/UnitTests/Helpers/PropertyNameHelperTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Dibware.StoredProcedureFramework.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dibware.StoredProcedureFramework.Tests.UnitTests.Helpers
+{
+    [TestClass]
+    public class PropertyNameHelperTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_WhenConstructedWithNullProperty_ThrowsException()
+        {
+            // ARRANGE
+
+            // ACT
+            new PropertyNameHelper(null);
+
+            // ASSERT
+        }
+    }
+}
