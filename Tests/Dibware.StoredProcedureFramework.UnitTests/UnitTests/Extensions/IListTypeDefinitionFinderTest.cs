0181e849a71b2a440162e6122c382c01321e650d
dibley1973 <dibley1973@yahoo.co.uk>
2015-12-19 05:40:05 +0000

    20151219_0539_DWLT Added `IListTypeDefinitionFinderTest` test class

---
+++ Tests/Dibware.StoredProcedureFramework.UnitTests/UnitTests/Extensions/IListTypeDefinitionFinderTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Dibware.StoredProcedureFramework.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dibware.StoredProcedureFramework.Tests.UnitTests.Extensions
+{
+    [TestClass]
+    public class IListTypeDefinitionFinderTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_WhenInstanceIsNull_ThrowsException()
+        {
+            // ARRANGE
+            IList list = null;
+
+            // ACT
+            new IListTypeDefinitionFinder(list);
+
+            // ASSERT
+        }
+
+        [TestMethod]
+        public void GetUnderlyingType_WhenInstanceIsValidList_ReturnsCorrectType()
+        {
+            // ARRANGE
+            var list = (IList)new List<ListItem>
+            {
+                new ListItem {}
+            };
+
+            // ACT
+            var actual = new IListTypeDefinitionFinder(list)
+                .GenericListType;
+
+            // ASSERT
+            Assert.AreEqual(typeof(ListItem), actual);
+        }
+
+        private class ListItem
+        {
+        }
+    }
+}
