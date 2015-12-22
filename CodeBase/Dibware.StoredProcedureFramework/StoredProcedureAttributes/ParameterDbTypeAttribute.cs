167c7e961ad9aadcc303e32c1b8f5ccc67d39c28
dibley1973 <dibley1973@yahoo.co.uk>
2015-12-22 08:40:34 +0000

    Added `ParameterDbTypeAttribute` back in with Obsolete marker attribute

---
+++ CodeBase/Dibware.StoredProcedureFramework/StoredProcedureAttributes/ParameterDbTypeAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Dibware.StoredProcedureFramework.StoredProcedureAttributes
+{
+    [Obsolete("This attribute has been superseeded by the {DbTypeAttribute} attribute. Please update your code accordingly", true)]
+    public class ParameterDbTypeAttribute : Attribute
+    {
+    }
+}
