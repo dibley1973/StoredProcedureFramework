ce898520846891016fd40fa74fc49729f2933b7c
dibley1973 <dibley1973@yahoo.co.uk>
2016-01-07 07:44:45 +0000

    20160107)0743_DWDT Upped assembly versions to 0.9 ready for any new enhancements or fixes

--- Tests/Dibware.StoredProcedureFramework.IntegrationTests.Database/Properties/AssemblyInfo.cs
+++ Tests/Dibware.StoredProcedureFramework.IntegrationTests.Database/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
